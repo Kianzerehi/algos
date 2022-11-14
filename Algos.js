af1ca163a03eb7aaee91fc3c81b4ec9d0c559fb4
Kianzerehi <107164517+Kianzerehi@users.noreply.github.com>
2022-11-14 12:53:53 -0800

    new algo

--- Algos.js
+++ Algos.js
@@ -56,3 +56,24 @@ var isPalindrome = function (x) {
 let toLowerCase = function (s) {
   return s.toLowerCase();
 }; //Easiest problem I've done
+
+/* Valid Palindrome
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  let regexPattern = /[^A-Za-z0-9]/g;
+  let palindrome = s
+    .toLowerCase()
+    .replace(regexPattern, "")
+    .split("")
+    .reverse()
+    .join("");
+  let originalString = s.toLowerCase().replace(regexPattern, "");
+  if (palindrome === originalString) {
+    return true;
+  } else {
+    return false;
+  }
+}; //I removed all the non-alphanmeric characters as instructed and then made sure all characters were lowercase.
+//I then split the characters into an array, reverse,then combined again. I made sure the original string was turned lowercase and removed all the non-alphanumeric numbers.I set up an 'if' statement to be sure the string returns true or false
