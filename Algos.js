b9797a46b328c1d0ef7918264f723e467a839b25
Kianzerehi <107164517+Kianzerehi@users.noreply.github.com>
2022-11-22 13:58:20 -0800

    anotha one

--- Algos.js
+++ Algos.js
@@ -90,3 +90,10 @@ var reverseString = function (s) {
   return s.reverse("");
 };
 //Self explanatory
+//2469. Convert the Temperature
+var convertTemperature = function (celsius) {
+  const kelvin = celsius + 273.15;
+  const fahrenheit = celsius * 1.8 + 32;
+  return (ans = [kelvin, fahrenheit]);
+};
+//Put in formula to translate celcius to other forms of temperature. returned the answer into an array
