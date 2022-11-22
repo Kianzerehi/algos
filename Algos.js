d74492e66c532628cb4cc9faf9a8b2a25346e683
Kianzerehi <107164517+Kianzerehi@users.noreply.github.com>
2022-11-22 13:39:52 -0800

    new algo

--- Algos.js
+++ Algos.js
@@ -77,3 +77,11 @@ var isPalindrome = function (s) {
   }
 }; //I removed all the non-alphanmeric characters as instructed and then made sure all characters were lowercase.
 //I then split the characters into an array, reverse,then combined again. I made sure the original string was turned lowercase and removed all the non-alphanumeric numbers.I set up an 'if' statement to be sure the string returns true or false
+
+//Length of Last Word
+var lengthOfLastWord = function (s) {
+  let removedEndSpace = s.trimEnd().split(" ");
+  let lastWord = removedEndSpace.length - 1;
+  return removedEndSpace[lastWord].length;
+};
+//I removed spaces at the end of the string and split it into an array. I found the zero based index of the last item in any array and found the length of it
