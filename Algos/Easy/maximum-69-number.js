c3da2e77babb8f02be30f04d4c7b47477d4ec3c3
Kianzerehi <107164517+Kianzerehi@users.noreply.github.com>
2022-11-22 17:12:21 -0800

    new algo

---
+++ Algos/Easy/maximum-69-number.js
@@ -0,0 +1,4 @@
+function maximum69Number(num) {
+  return Number(num.toString().replace("6", "9"));
+}
+//needed help with this problem. I originally ran a for loop but then realized i needed to turn the number into an array. However when the string numbers were seperated, I had trouble turning it into a number again
